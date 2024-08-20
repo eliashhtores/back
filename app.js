11965d468cf79f59c2ee7f7614b918c48143b1dd
eliashhtores <eliashhtores@github.com>
2024-08-20 16:58:06 -0600

    Added support for Stripe

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ import userRoutes from "./routes/user.js"
 import courseRoutes from "./routes/course.js"
 import sessionRoutes from "./routes/session.js"
 import registrationRoutes from "./routes/registration.js"
-
+import Stripe from "stripe"
 dotenv.config()
 
 const app = express()
@@ -14,6 +14,31 @@ const app = express()
 app.use(express.json())
 app.use(bodyParser.json())
 app.use(cors())
+
+const stripe = new Stripe("sk_test_IKYCHOAmUhC7IPTdaoVtO58D")
+const calculateOrderAmount = (items) => {
+    let total = 0
+    items.forEach((item) => {
+        total += item.amount
+    })
+    return total
+}
+
+app.post("/create-payment-intent", async (req, res) => {
+    const { items } = req.body
+
+    const paymentIntent = await stripe.paymentIntents.create({
+        amount: calculateOrderAmount(items),
+        currency: "mxn",
+        automatic_payment_methods: {
+            enabled: true,
+        },
+    })
+
+    res.send({
+        clientSecret: paymentIntent.client_secret,
+    })
+})
 
 app.use("/user", userRoutes)
 app.use("/course", courseRoutes)
